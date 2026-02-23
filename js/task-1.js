b0479e7fa2a79ac718634998a9e025fcd5faf36f
Maria Khomynets <masha.khomynets@gmail.com>
2026-02-23 13:26:26 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -5,33 +5,3 @@ function makeTransaction(quantity, pricePerDroid) {
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-
-function checkFileExtension(fileName, ext) {
-  const messageName = fileName.endsWith(ext);
-
-  if (messageName) {
-    return 'File extension matches';
-  } else {
-    return 'File extension does not match';
-  }
-}
-console.log(checkFileExtension('styles.css', '.js'));
-console.log(checkFileExtension('styles.css', '.css'));
-console.log(checkFileExtension('app.js', '.js'));
-console.log(checkFileExtension('app.js', '.html'));
-console.log(checkFileExtension('index.html', '.html'));
-console.log(checkFileExtension('index.html', '.html'));
-
-function getFileName(file) {
-  const mes = file.indexOf('.');
-
-  if (mes === -1) {
-    return file;
-  } else {
-    return file.slice(0, mes);
-  }
-}
-
-console.log(getFileName('styles.css'));
-console.log(getFileName('app.js'));
-console.log(getFileName('app'));
